64cd2b899781850aa7d53d5c20284b8ab66da6af
Brian Worthington <worthington.brian@realtyts.com>
2025-07-15 14:46:05 -0500

    Fix syntax errors: Add missing opening curly braces after namespace declarations

--- Backend/Services/FileValidationService.cs
+++ Backend/Services/FileValidationService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Backend.Services
+{
 
     /// <summary>
     /// Implementation of file validation service
--- Backend/Services/RequestDiagnosticsService.cs
+++ Backend/Services/RequestDiagnosticsService.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Backend.Services
+{
 
     /// <summary>
     /// Implementation of the request diagnostics service
